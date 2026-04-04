7e392ee4fdf2bb303f740e0f42d5311534340ecc
OleksiiMedvedenko <medvedenko@tkomp.pl>
2026-04-04 19:04:47 +0200

    configuration fixes

--- src/app/api/client.ts
+++ src/app/api/client.ts
@@ -1,8 +1,17 @@
 import { useAuthStore } from "@/app/store/auth.store";
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+if (!baseURL) {
+  throw new Error("Missing VITE_API_BASE_URL in environment variables.");
+}
+
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 apiClient.interceptors.request.use((config) => {
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { AppProviders } from "@/app/providers/app-providers";
 import "@/i18n";
-import "@/styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
